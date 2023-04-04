ce5f4a6cf90cada24b4e22f261677d67f82672bb
Jérémy Jalladeau <hitgame444@gmail.com>
2023-04-04 20:00:29 +0200

    Correction de la navbar

--- js/shoppaysCapyfoot.js
+++ js/shoppaysCapyfoot.js
@@ -1,13 +1,12 @@
-
 window.addEventListener('DOMContentLoaded', event => {
     var navbarShrink = function () {
         const navbarCollapsible = document.body.querySelector('#mainNav');
         if (!navbarCollapsible) {
             return;
         }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.add('navbar-shrink')
-        } else if (window.scrollY !== 0) {
+        if (window.scrollY == 0 && window.scrollY !== 0  ) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
             navbarCollapsible.classList.add('navbar-shrink')
         }
 
@@ -19,6 +18,25 @@ window.addEventListener('DOMContentLoaded', event => {
     document.addEventListener('scroll', navbarShrink);
 
 
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
 
+
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
 
 });
